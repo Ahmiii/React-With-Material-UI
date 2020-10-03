50de99c91db38a26deb01a5e6501e065f9ae1b62
Ahmiii <ahmii.shaikh18@gmail.com>
2020-10-03 22:33:39 +0500

    populating popup

--- src/components/controls/PopUp.jsx
+++ src/components/controls/PopUp.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { Dialog, DialogContent } from "@material-ui/core";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  dialogeWrapper: {
+    padding: theme.spacing(2),
+    position: "absolute",
+    top: theme.spacing(5),
+  },
+}));
 
 const PopUp = (props) => {
-  const { title, children, openPopup, setopenPopup } = props;
+  const classes = useStyles();
+  const { title, children, openPopup } = props;
   return (
     <div>
-      <Dialog>
+      <Dialog
+        open={openPopup}
+        maxWidth="md"
+        classes={{ paper: classes.dialogeWrapper }}
+      >
         <DialogTitle>{title}</DialogTitle>
         <DialogContent>{children}</DialogContent>
       </Dialog>
